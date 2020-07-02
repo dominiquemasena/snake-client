cf2b9389760cbfb3e413d622ed67f58095470922
Dominique Masena <dominique.masena@live.ca>
2020-07-02 00:29:21 +0000

    client.js

--- client.js
+++ client.js
@@ -4,12 +4,15 @@ const connect = function() {
     host: '135.23.222.148',
     port: 50541
   });
-  console.log("connection created.")
-  // interpret incoming data as text
+
   conn.setEncoding('utf8'); 
+
   conn.on("connect", (connect) => {
     console.log("Successfully connected to game server");
-  })
+  });
+  conn.on("connect", () => {
+    conn.write('Name: DGM');
+  });
   conn.on("data", (data) => {
     console.log(data);
   })
@@ -18,9 +21,9 @@ const connect = function() {
   return conn;
 };
 
-connect();
-
 
 module.exports = {connect}
+
+
 
 
